3c3565b564c38417584074c9a34fb8e2e5aea481
Luis Hernández <ldhdv95@gmail.com>
2018-09-03 08:18:31 -0500

    Add Event vote now

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+class State {
+  constructor(size) {
+    this.state = {};
+    this.total = [];
+    this.positive = [];
+
+    for (let i = 0; i<size; i++) {
+      this.state[`${i}`] = {};
+      this.total[`${i}`] = 1;
+      this.positive[`${i}`] = 1;
+    }
+  }
+  
+  positiveVoting(index) {
+    this.total[index]= this.total[index] + 1;
+    this.positive[index] = this.positive[index] + 1;
+    this.state[index]['positive'] = this.positive[index];
+    this.state[index]['total'] = this.total[index];
+
+    return (this.positive[index]/this.total[index])*100
+  }
+
+  negativeVoting(index) {
+    this.total[index] = this.total[index] + 1;
+    this.state[index]['total'] = this.total[index];
+
+    return (this.positive[index]/this.total[index])*100
+  }
+}
+
 
 /* DOM Events */
 
@@ -25,5 +55,36 @@ for (let i = 0; i<thumbs_up.length; i++) {
   thumbs_down[i].addEventListener('click', function(e) {
     e.currentTarget.classList.add('active');
     thumbs_up[i].classList.remove('active');
+  })
+}
+
+//Event for Vote now
+//const vote_again = document.getElementsByClassName('button__inline--again');
+const vote_now = document.getElementsByClassName('button__inline--now');
+const like = document.getElementsByClassName('progress__like');
+const dislike = document.getElementsByClassName('progress__dislike');
+const bar = document.getElementsByClassName('progress__value');
+let value = new State(bar.length);
+
+for (let i = 0; i<vote_now.length; i++) {
+  vote_now[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    if (thumbs_up[i].classList.contains('active')) {
+
+      let percentage = value.positiveVoting(i);
+      bar[i].style.width = `${percentage}%`;
+      like[i].innerHTML = `${Math.round(percentage)}%`
+      dislike[i].innerHTML = `${Math.round(100 - percentage)}%`;
+      
+    } else if (thumbs_down[i].classList.contains('active')) {
+
+      let percentage = value.negativeVoting(i);
+      bar[i].style.width = `${percentage}%`;
+      dislike[i].innerHTML = `${Math.round(100 - percentage)}%`;
+      like[i].innerHTML = `${Math.round(percentage)}%`;
+    }
+
+    
   })
 }
