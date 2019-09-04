84f85b11b48b442286fc1c40b9925adc9f4eeeca
Luis Hernández <ldhdv95@gmail.com>
2019-09-04 01:52:16 -0500

    update: Change navbar layout and interaction

--- js/app.js
+++ js/app.js
@@ -129,18 +129,17 @@ window.addEventListener("load", function(e) {
   }
 });
 
-//Event for the navbar (toggle burger)
+//Event for the navbar
 const navRight = document.getElementsByClassName(
   "navbar__section navbar__section--right"
 );
 
 document.getElementById("burger").addEventListener("click", function(e) {
   navRight[0].classList.toggle("active__block");
-  if (navRight[0].classList.contains("active__block")) {
-    this.firstElementChild.setAttribute("src", "images/white_close.svg");
-  } else {
-    this.firstElementChild.setAttribute("src", "images/burger.svg");
-  }
+});
+
+document.getElementById("close").addEventListener("click", function(e) {
+  navRight[0].classList.toggle("active__block");
 });
 
 //Event Select vote
