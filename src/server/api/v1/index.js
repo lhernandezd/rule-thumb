7cb620531107404a68830eafea145e80417aed5a
Luis Hernández <ldhdv95@gmail.com>
2019-09-05 10:20:46 -0500

    update: New Model and crud for users - Add model for users - Add controller for users - Add routes for users

--- src/server/api/v1/index.js
+++ src/server/api/v1/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const tasks = require('./candidates/routes');
+const users = require('./users/routes');
 
 router.use('/candidates', tasks);
+router.use('/users', users);
 
 module.exports = router;
