f70467af99f76cf3f223719e0f199d1f128d3be8
Luis Hernández <ldhdv95@gmail.com>
2019-09-05 04:34:35 -0500

    update: Model for candidates, candidates controller database query - Add model for candidates - Add param doc for routes - Update candidates controller with database queries

--- src/server/api/v1/candidates/controller.js
+++ src/server/api/v1/candidates/controller.js
@@ -1,33 +1,91 @@
-exports.create = (req, res, next) => {
-  const { body = {} } = req;
-  res.json(body);
+const { Model } = require('./model');
+
+exports.id = async (req, res, next, id) => {
+  try {
+    const doc = await Model.findById(id);
+    if (doc) {
+      req.doc = doc;
+      next();
+    } else {
+      next({
+        statusCode: 404,
+        message: 'Resource not found',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.create = async (req, res, next) => {
+  const { body = {}, params = {} } = req;
+
+  try {
+    const doc = await Model.create({ ...body, ...params });
+    res.status(201);
+    res.json({
+      data: doc,
+      success: true,
+      statusCode: 201,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.all = (req, res, next) => {
-  res.json([]);
+  Model.find().exec((err, docs) => {
+    if (err) {
+      next(err);
+    } else {
+      res.json({
+        data: docs,
+        success: true,
+        statusCode: 200,
+      });
+    }
+  });
 };
 
 exports.read = (req, res, next) => {
-  const { params = {} } = req;
-  const { id = 1 } = params;
+  const { doc = {} } = req;
   res.json({
-    id,
+    data: doc,
+    success: true,
+    statusCode: 200,
   });
+
 };
 
-exports.update = (req, res, next) => {
-  const { params = {}, body = {} } = req;
-  const { id = 1 } = params;
-  res.json({
-    id,
-    ...body,
-  });
+exports.update = async (req, res, next) => {
+  const { body = {}, doc = {} } = req;
+
+  try {
+    Object.assign(doc, body);
+    const updated = await doc.save();
+
+    res.json({
+      data: updated,
+      success: true,
+      statusCode: 200,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.delete = (req, res, next) => {
-  const { params = {} } = req;
-  const { id = 1 } = params;
-  res.json({
-    id,
-  });
+exports.delete = async (req, res, next) => {
+  const { doc = {} } = req;
+
+  try {
+    const deleted = await doc.remove();
+
+    res.json({
+      data: deleted,
+      success: true,
+      statusCode: 200,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
