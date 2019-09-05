c880a36433d82c9d915b59ce45c5389c48f773cd
Luis Hernández <ldhdv95@gmail.com>
2019-09-05 03:24:52 -0500

    update: Add custom logs - Add Winston: Logging library - Add Morgan: Log formatter - Add requestId: uuid generator for logs - Add strip-final-newline: newline for standard output

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
+const requestId = require('express-request-id')();
+const logger = require('./config/logger');
 const app = express();
 
 const api = require('./api/v1');
+
+// Middleware for logs
+app.use(requestId);
+app.use(logger.requests);
+
 app.use('/api/v1', api);
 app.use('/api', api);
 
 // Not route found middleware
 app.use((req, res, next) => {
-  res.status(404);
-  res.json({
-    message: 'Resource not found',
+  const statusCode = 404;
+  const message = 'Route not found';
+
+  next({
+    message,
+    statusCode,
   });
 });
 
 // Error middleware
 app.use((err, req, res, next) => {
   const { message, statusCode = 500 } = err;
+  
   res.status(statusCode);
   res.json({
     message,
