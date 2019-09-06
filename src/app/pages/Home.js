e1210d2f573b643312be61913964c72801066e4a
Luis Hernández <ldhdv95@gmail.com>
2019-09-05 22:24:00 -0500

    update: Add Message component

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,15 +1,20 @@
 import React, { Fragment } from 'react'
-import { Link } from "react-router-dom";
-import Header from '../components/Header.js';
+import Header from '../components/Header';
+import Message from '../components/Message';
 
 export default function Home() {
   return (
     <Fragment>
       <Header />
-      Home
-      <Link to='/auth'>
-      Go to Auth
-      </Link>
+      <div className='container'>
+        <Message 
+          title='Speak out. Be heard.'
+          subtitle='Be counted'
+          description={`Rule of Thumb is a crowd sourced court of public opinion where anyone
+          and everyone can speak out and speak freely. It's easy: You share your
+          opinion, we analyze and put the data in a public report.`}
+        />
+      </div>
     </Fragment>
   )
 }
