a31b56874e391bf0e832f92e29579a28bf76f912
Luis Hernández <ldhdv95@gmail.com>
2019-09-06 08:53:24 -0500

    update: add new pages

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,9 @@ import React, { Fragment } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import How from './pages/How';
+import PastTrials from './pages/PastTrials';
+
 import './scss/main.scss';
 
 export default function App() {
@@ -11,6 +14,8 @@ export default function App() {
         <Route path='/' exact component={Home} />
         <Route path='/home' component={Home} />
         <Route path='/auth' component={Auth} />
+        <Route path ='/how' component={How} />
+        <Route path ='/pastTrials' component={PastTrials} />
         <Redirect to='/' />
       </Switch>
     </Fragment>
--- src/app/pages/PastTrials.js
+++ src/app/pages/PastTrials.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
     <div className='container'>
-      <h1>Auth</h1>
+      <h1>Past Trials</h1>
       <NavLink className='navigation__link' to='/home'>
         <p>Go to Home</p>
       </NavLink>
